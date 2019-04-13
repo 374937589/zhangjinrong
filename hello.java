73c3308dcd3f3ae144d6b374500e783c7a923452
374937589 <374937589@qq.com>
2019-04-13 11:43:29 +0800

    Initial commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+package com.bdqn.zjr;
+
+/**
+ * Created by Administrator on 2019/4/13.
+ */
+public class hello {
+    public static void main(String [] args){
+        System.out.println("你好 张津榕儿");
+    }
+}
